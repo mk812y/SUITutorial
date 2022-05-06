80d7559a2245e5fec3e5c12988ebf4df01bd4c2d
mk812y <mk812y@gmail.com>
2022-05-06 17:18:19 +0300

    LandmarkRow

---
+++ SUITutorial/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  SUITutorial
+//
+//  Created by Михаил Куприянов on 06.05.2022.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
