a788f24a11af7a3c32d75194c60648c97f4cc299
mk812y <mk812y@gmail.com>
2022-05-09 22:34:03 +0300

    Add an Edit Mode

---
+++ SUITutorial/Model/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  SUITutorial
+//
+//  Created by Михаил Куприянов on 09.05.2022.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var preferNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { rawValue }
+    }
+
+}
---
+++ SUITutorial/Views/Profile/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  SUITutorial
+//
+//  Created by Михаил Куприянов on 09.05.2022.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
